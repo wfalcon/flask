0a0dbf62f98bf6ba73955a7223519900b822d430
Влад <wfalcon@mail.ru>
2020-01-08 13:24:05 +0300

    Add files via upload

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, escape, session
+from flask import Flask, render_template, request, redirect, escape, session, copy_current_request_context
 from vsearch import search4letters
 from DBcm import UserDatabase, ConnectionError, CredentialsError
 from config import dbconfig, secret_key
 from checker import check_logged_in
+from threading import Thread
+
 
 app = Flask(__name__)
 app.config['dbconfig'] = dbconfig
@@ -16,18 +18,20 @@ def entry_page() -> 'html':
 		the_title='Добро пожаловать в web поиск букв в заданной фразе!')
 
 
-def log_request(req: 'flask_request', res: str) -> None:
-	with UserDatabase(app.config['dbconfig']) as cursor:
-		_SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s) """
-		cursor.execute(_SQL, (req.form['phrase'],
-							req.form['letters'],
-							req.remote_addr,
-							req.user_agent.browser,
-							res, ))
-	
-
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
+
+	@copy_current_request_context
+	def log_request(req: 'flask_request', res: str) -> None:
+		with UserDatabase(app.config['dbconfig']) as cursor:
+			_SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s) """
+			cursor.execute(_SQL, (req.form['phrase'],
+								req.form['letters'],
+								req.remote_addr,
+								req.user_agent.browser,
+								res, ))
+
+
 	title = 'Результат поиска'
 	phrase = request.form['phrase']
 	letters = request.form['letters']
@@ -38,7 +42,8 @@ def do_search() -> str:
 		results = 'Не найдено'
 
 	try:
-		log_request(request, results)
+		logreg = Thread(target=log_request, args=(request, results))
+		logreg.start()
 	except Exception as err:
 		print('*****Ошибка подключения к БД:  ', str(err))
 
